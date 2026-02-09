b65445ff0e8ba5b4d08890169b7aa5b09d5e7933
dingus <dingus@schlingus.cc>
2026-02-09 08:57:27 +0100

    bil + sketen hud

--- scenes/cars/vehicle_body_3d.gd
+++ scenes/cars/vehicle_body_3d.gd
@@ -2,7 +2,7 @@ extends VehicleBody3D
 
 signal exit_car
 
-@export var engine_force_amount: float = 200.0
+@export var engine_force_amount: float = 600.0
 @export var brake_force_amount: float = 50.0
 @export var steering_angle: float = 0.4
 
@@ -11,26 +11,28 @@ signal exit_car
 var in_car: bool = false
 
 func _physics_process(delta: float) -> void:
+	print(in_car)
 	var throttle := 0.0
 	var steer := 0.0
-	if Input.is_action_pressed("ui_up"):
-		throttle = engine_force_amount
-	elif Input.is_action_pressed("ui_down"):
-		throttle = -engine_force_amount / 1.5
+	if camera_3d.current:
+		if Input.is_action_pressed("ui_up"):
+			throttle = engine_force_amount
+		elif Input.is_action_pressed("ui_down"):
+			throttle = -engine_force_amount
 
-	if Input.is_action_pressed("ui_left"):
-		steer = steering_angle
-	elif Input.is_action_pressed("ui_right"):
-		steer = -steering_angle
+		if Input.is_action_pressed("ui_left"):
+			steer = steering_angle
+		elif Input.is_action_pressed("ui_right"):
+			steer = -steering_angle
 		
-	if Input.is_action_just_pressed("f"):
-		in_car = false
-		camera_3d.current = false
-		exit_car.emit()
-	if Input.is_action_pressed("reset"):
-		rotation.x = 0
-		rotation.z = 0
-		position.y += 1
+		if Input.is_action_just_pressed("f"):
+			in_car = false
+			camera_3d.current = false
+			exit_car.emit()
+		if Input.is_action_just_pressed("reset"):
+			rotation.x = 0
+			rotation.z = 0
+			position.y += 1
 
 	engine_force = throttle
 	brake = brake_force_amount if Input.is_action_pressed("ui_select") else 0.0
